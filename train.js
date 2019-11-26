a7fd322bb6ca4e99694b2a837d858cd3c2de7b65
Tatiana <tgilde14@gmail.com>
2019-11-26 12:24:44 -0500

    train

--- train.js
+++ train.js
@@ -1,80 +1,95 @@
-$(document).ready(function () {
 
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyA4Y6Ywc3MMHCQ7tZHkMyH1l9vx_qSRA6M",
-      authDomain: "train-schedule-5f60e.firebaseapp.com",
-      databaseURL: "https://train-schedule-5f60e.firebaseio.com/",
-      storageBucket: "train-schedule-5f60e.appspot.com",
-      messagingSenderId: 287123784516
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBhyfYCPnftEg2NYeFbY-9EDjP6e4hLE8o",
+    authDomain: "train-schedule-a705a.firebaseapp.com",
+    databaseURL: "https://train-schedule-a705a.firebaseio.com",
+    projectId: "train-schedule-a705a",
+    storageBucket: "train-schedule-a705a.appspot.com",
+    messagingSenderId: "815073667281",
+    appId: "1:815073667281:web:2848e94e42fa01bc22fdf0",
+    measurementId: "G-9MPXBRWXN6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+var database = firebase.database();
+
+
+//Variable for current time
+var currentTime = moment(new Date());
+currentTime = moment().format('HH:mm');
+
+//Button for adding Trains
+$("#add-train").on("click", function() {
+
+    // Grab user input from text input field
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#train-destination-input").val().trim();
+    var trainTime = $("#train-time-input").val().trim();
+    var trainFrequency = parseInt($("#train-frequency-input").val().trim());
+
+
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
     };
-    firebase.initializeApp(config);
-  
-    var database = firebase.database();
-  
-    // Capture Button Click
-    $("#addTrain").on("click", function (event) {
-      event.preventDefault();
-  
-      // Grabbed values from text boxes
-      var trainName = $("#trainName").val().trim();
-      var destination = $("#destination").val().trim();
-      var firstTrain = $("#firstTrain").val().trim();
-      var freq = $("#interval").val().trim();
-  
-      // Code for handling the push
-      database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: freq
-      });
-    });
-  
-  
-    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-    database.ref().on("child_added", function (childSnapshot) {
-  
-      var newTrain = childSnapshot.val().trainName;
-      var newLocation = childSnapshot.val().destination;
-      var newFirstTrain = childSnapshot.val().firstTrain;
-      var newFreq = childSnapshot.val().frequency;
-  
-      // First Time (pushed back 1 year to make sure it comes before current time)
-      var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
-  
-      // Current Time
-      var currentTime = moment();
-  
-      // Difference between the times
-      var diffTime = moment().diff(moment(startTimeConverted), "minutes");
-  
-      // Time apart (remainder)
-      var tRemainder = diffTime % newFreq;
-  
-      // Minute(s) Until Train
-      var tMinutesTillTrain = newFreq - tRemainder;
-  
-      // Next Train
-      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-      var catchTrain = moment(nextTrain).format("HH:mm");
-  
-      // Display On Page
-      $("#all-display").append(
-        ' <tr><td>' + newTrain +
-        ' </td><td>' + newLocation +
-        ' </td><td>' + newFreq +
-        ' </td><td>' + catchTrain +
-        ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
-  
-      // Clear input fields
-      $("#trainName, #destination, #firstTrain, #interval").val("");
-      return false;
-    },
-      //Handle the errors
-      function (errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
-  
-  }); //end document ready
-  
+
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+    // Alert
+    alert("Train successfully added");
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#train-destination-input").val("");
+    $("#train-time-input").val("");
+    $("#train-frequency-input").val("");
+
+    // Prevents loading a new page
+    return false;
+});
+
+// 3. Creates Firebase event for adding train to the database and a row in the html when a user adds a train
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    // Store train data into  variables
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    // Train time converted
+    var trainTimeConverted = moment(childSnapshot.val().time, "HH:mm");
+    console.log(trainTimeConverted);
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    console.log(diffTime);
+    // Time apart (remainder)
+    var tRemainder = diffTime % childSnapshot.val().frequency;
+    console.log(tRemainder);
+    // Minutes away Until Train Arrival
+    var minutesAway = childSnapshot.val().frequency - tRemainder;
+    console.log(minutesAway);
+    // Next Train Arrival
+    var nextTrainArrival = moment().add(minutesAway, "minutes");
+    var nextArrivalConverted = moment(nextTrainArrival).format("HH:mm");
+    console.log(nextArrivalConverted);
+    //Add train times to train schedule
+    var row = $('<tr>');
+    $(row).append($('<td>').text(childSnapshot.val().name));
+    $(row).append($('<td>').text(childSnapshot.val().destination));
+    $(row).append($('<td>').text(childSnapshot.val().frequency));
+    $(row).append($('<td>').text(nextArrivalConverted));
+    $(row).append($('<td>').text(minutesAway));
+    $('#table-body-train-schedule').append(row);
+    // Handles any errors
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
